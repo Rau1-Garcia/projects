20a07d4628faa48260849aee2a278c47243c36c5
Rau1-Garcia <garciadasilvaraul@gmail.com>
2024-11-20 19:40:18 -0300

    Add files via upload

---
+++ urna eleitoral com if.py
@@ -0,0 +1,63 @@
+
+print("Olá bem-vinto a urna eleitoral")
+
+prefeito=int(input("digite numero do candidato(a) a prefeito: "))
+print("\n")
+print("\n")
+vereador=int(input("digite o numero do candidato(a) a vereador: "))
+print("\n")
+print("\n")
+if prefeito==22:
+         print("obrigado pelo voto,o senhor votou no Padre Osvaldo")
+
+
+if prefeito==13:
+         print("obrigado pelo voto,o senhor votou na Beth Sahão")
+
+
+
+
+if prefeito==55:
+         print("obrigado pelo voto,o senhor votou no Ricardo Rebelato ")
+
+
+if prefeito==28:
+         print("obrigado pelo voto,o senhor votou no  Pablo marçal")
+
+
+if prefeito==45:
+         print("obrigado pelo voto,o senhor votou no Datena")
+
+
+
+
+
+
+if vereador==12555:
+
+            print("obrigado pelo voto, senhor votou no Robson franizzo")
+
+
+if vereador== 22788:
+
+            print("obrigado pelo voto, senhor votou no marcelo bonfatino")
+
+
+if vereador==55677:
+
+             print("obrigado pelo voto, senhor votou no douglas bragantino")
+
+
+if vereador==25987:
+
+             print("obrigado pelo voto, senhor votou no  pedro gonçalves")
+
+
+if vereador==12565:
+
+             print("obrigado pelo voto, senhor votou no Ricardo pereira")
+
+
+
+
+
